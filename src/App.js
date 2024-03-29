7c92e127790bccaeecad24f099cc00a960c8255e
vaibhav rawal <vaibhavr@gmail.com>
2024-03-29 23:33:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Horse from "./component/horse";
 
 function App() {
   return (
     <div className="App">
-     
+      <Horse />
     </div>
   );
 }
